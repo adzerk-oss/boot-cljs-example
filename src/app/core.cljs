f583fc947819baa3d82330b546e32b7f23b31412
Hoàng Minh Thắng <p@banphim.net>
2015-03-31 11:13:32 +0700

    rename ns app to app.core

---
+++ src/app/core.cljs
@@ -0,0 +1,6 @@
+(ns app.core)
+
+(defn main []
+  (let [c (.. js/document (createElement "DIV"))]
+    (aset c "innerHTML" "<p>i'm dynamically created</p>")
+    (.. js/document (getElementById "container") (appendChild c))))
